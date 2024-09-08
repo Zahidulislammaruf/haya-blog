5f7151f3cb18c5f982154d67173b91b28b3ee33a
Maruf <zahidulmaruf13@gmail.com>
2024-09-08 21:43:43 +0600

    Feature Section Added

---
+++ src/Component/Featured/Featured.jsx
@@ -0,0 +1,23 @@
+
+const Featured = () => {
+    return (
+        <div className=" w-2/4 mx-auto mt-12">
+            <div className="card lg:card-side bg-base-100 shadow-xl">
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"
+      alt="Album" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">New album is released!</h2>
+    <p>Click the button to listen on Spotiwhy app.</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Listen</button>
+    </div>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Featured;
--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,9 +1,11 @@
 import BannerWithNav from "../BannerWithNav/BannerWithNav";
+import Featured from "../Featured/Featured";
 
 const Home = () => {
     return (
         <div>
             <BannerWithNav></BannerWithNav>
+            <Featured></Featured>
         </div>
     );
 };
