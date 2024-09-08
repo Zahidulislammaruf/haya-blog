27497dd78a3edacb4bba4868a67081b790f12055
Maruf <zahidulmaruf13@gmail.com>
2024-09-08 21:31:28 +0600

    Banner Search Box And Populer Tag Added

--- src/Component/BannerWithNav/Banner.jsx
+++ src/Component/BannerWithNav/Banner.jsx
@@ -7,16 +7,19 @@ const Banner = () => {
   style={{
     backgroundImage: "url(https://i.ibb.co.com/h8MRHYf/pexels-cottonbro-6209562.jpg)",
   }}>
-  <div className="hero-overlay bg-opacity-30"></div>
-  <div className="hero-content text-neutral-content text-center">
-    <div className="max-w-md">
-      <h2 className=" text-[48px]">Our Classroom</h2>
-      <label className="input input-bordered flex items-center gap-2">
-         <input type="text" className="grow" placeholder="Search" />
-          <span className=" p-2 rounded  bg-black text-white">Search</span>
-      </label>
-      <button className="btn btn-primary">Get Started</button>
-    </div>
+  <div className="hero-overlay  bg-opacity-30"></div>
+  <div>
+    <h2 className=" text-[48px] text-center text-white">Our Classroom</h2>
+    <label className="input input-bordered flex items-center gap-2">
+  <input type="text" className="grow w-48 md:w-96 " placeholder="Search" />
+  <span className="badge badge-info">Search</span>
+</label>
+<div className="flex text-white gap-6 mt-4">
+  <p>Populer Tags : </p>
+  <p className=" border border-white bg-white bg-opacity-40 p-1 px-2 text-black font-semibold"><a href="">Desing</a></p>
+  <p className=" border border-white bg-white bg-opacity-40 p-1 px-2 text-black font-semibold"><a href="">Sports</a></p>
+  <p className=" border border-white bg-white bg-opacity-40 p-1 px-2 text-black font-semibold"><a href="">Weather</a></p>
+</div>
   </div>
 </div>  
         </div>
