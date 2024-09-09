979c458446cc25076845c30fa7d0872da09b12a6
Maruf <zahidulmaruf13@gmail.com>
2024-09-09 10:04:44 +0600

    React Icon Added

--- src/Component/Popular/Populer.jsx
+++ src/Component/Popular/Populer.jsx
@@ -1,16 +1,18 @@
+import { IoMdArrowForward } from "react-icons/io";
 
 
 const Populer = () => {
     return (
-        <div>
+        <div className=" container mx-auto">
             <div>
                 <div className="">
                     <h2 className="text-[36px] font-semibold">Popular Articles</h2>
                     <h2>We share common trends, strategies ideas, opinions, short & long stories from the team behind company.</h2>
                 </div>
-                <h2>
-
-                </h2>
+                <div>
+                    <button className="btn bg-transparent border border-blue-600">View All<IoMdArrowForward />
+                    </button>
+                </div>
             </div>
             
         </div>
