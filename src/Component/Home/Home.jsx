754358fb9d26b771840ebd49b46bf07833cbcf85
Maruf <zahidulmaruf13@gmail.com>
2024-09-09 09:54:51 +0600

    Popular Articles Section Added

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,11 +1,13 @@
 import BannerWithNav from "../BannerWithNav/BannerWithNav";
 import Featured from "../Featured/Featured";
+import Populer from "../Popular/Populer";
 
 const Home = () => {
     return (
         <div>
             <BannerWithNav></BannerWithNav>
             <Featured></Featured>
+            <Populer></Populer>
         </div>
     );
 };
---
+++ src/Component/Popular/Populer.jsx
@@ -0,0 +1,20 @@
+
+
+const Populer = () => {
+    return (
+        <div>
+            <div>
+                <div className="">
+                    <h2 className="text-[36px] font-semibold">Popular Articles</h2>
+                    <h2>We share common trends, strategies ideas, opinions, short & long stories from the team behind company.</h2>
+                </div>
+                <h2>
+
+                </h2>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Populer;
