535333155eeb5112ad27d2d2aa7e3b76f5a06b00
Maruf <zahidulmaruf13@gmail.com>
2024-09-09 09:49:31 +0600

    Feature Section Finished

--- src/Component/Featured/Featured.jsx
+++ src/Component/Featured/Featured.jsx
@@ -1,20 +1,22 @@
 
 const Featured = () => {
     return (
-        <div className=" w-3/4 mx-auto mt-12">
+        <div className=" w-3/4 mx-auto mt-12 mb-12">
             <div className="card card-side bg-base-100 shadow-xl">
-  <figure>
+  <figure className="flex-1">
     <img
       src="https://i.ibb.co.com/n0bGpcp/Image.png"
       alt="Album" />
   </figure>
-  <div className=" p-4">
+  <div className=" p-4 flex-1">
      <p className=" badge badge-primary">Feature</p>
-     <h2 className=" text-[16px] lg:text-[38px] font-semibold">Cheap Airline Tickets Great Ways To Save</h2>
-     <p>
+    <div className="flex flex-col mt- lg:mt-12">
+    <h2 className=" underline cursor-pointer text-[20px] lg:text-[38px] font-semibold">Cheap Airline Tickets Great  Ways <br /> <span className="text-red-500">To Save</span> </h2>
+     <p className=" lg:mt-6 ">
      In this digital generation where information can be easily obtained within seconds, business cards ...  
      </p>
-     <div className=" flex justify-between mt-[16px] ">
+    </div>
+     <div className=" flex justify-between mt-4 lg:mt-12 ">
         <div className=" flex gap-4">
         <div className="avatar">
   <div className="w-14 rounded-full">
