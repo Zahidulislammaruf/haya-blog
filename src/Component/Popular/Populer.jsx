c29be70c36375612db67769ed1ed70287fabfe1c
Maruf <zahidulmaruf13@gmail.com>
2024-09-10 20:22:30 +0600

    Feature Section Finished

--- src/Component/Popular/Populer.jsx
+++ src/Component/Popular/Populer.jsx
@@ -11,7 +11,7 @@ const Populer = () => {
             <button className="btn bg-transparent border border-blue-600 hover:bg-transparent hover:border-blue-500">View All<IoMdArrowForward />
             </button>
             </div>
-            <div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 rounded-lg">
             <div className=" text-white bg-cover bg-no-repeat rounded  bg-base-100 image-full  shadow-xl" style={{
     backgroundImage: "url(https://i.ibb.co.com/W5NkcNp/Image-2.png)",
   }}>
@@ -21,6 +21,35 @@ const Populer = () => {
                     <div className=" pt-[65%]">
 
                     <h2 className=" text-[36px] shadow-md font-semibold">How to prevent and protect your family from carbon monoxide</h2>
+                    <div className=" flex justify-between mt-4 lg:mt-12 ">
+        <div className=" flex gap-4">
+        <div className="avatar">
+  <div className="w-14 rounded-full">
+    <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+  </div>
+         </div>
+         <div>
+            <h2 className=" text-[20px] font-mono font-semibold">Miss Diana</h2>
+            <h2 className="badge badge-info">Verified</h2>
+         </div>
+        </div>
+        <div>
+            <p className=" font-thin">4:55 PM</p>
+            <p className=" font-thin">02 May</p>
+        </div>
+     </div>
+                    </div> 
+                </div>
+           </div>
+            <div className=" text-white bg-cover bg-no-repeat rounded  bg-base-100 image-full  shadow-xl" style={{
+    backgroundImage: "url(https://i.ibb.co.com/BLznrP5/Image-3.png)",
+  }}>
+
+                 <div className=" p-[5%]">
+                 <p className=" badge bg-white text-[18px] text-green-500  font-semibold">Feature</p>
+                    <div className=" pt-[65%]">
+
+                    <h2 className=" text-[36px] shadow-md font-semibold">Motherhood is the hardest and the best job ever</h2>
                     <div className=" flex justify-between mt-4 lg:mt-12 ">
         <div className=" flex gap-4">
         <div className="avatar">
